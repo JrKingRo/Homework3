873bff8ba919e22b930b0b89391ead9bf0bad18f
JrKingRo <roma1299.roma@mail.ru>
2022-07-28 18:41:28 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+*/
+
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    {
+        Console.WriteLine($"{i}*{i}*{i} ={i*i*i}");
+    }
+}
+
+
+
+
+
+
