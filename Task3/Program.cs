41cdc449a494e60c20c43289520c89ee0fabd4f0
JrKingRo <roma1299.roma@mail.ru>
2022-08-08 14:05:43 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,3 +7,23 @@
 Console.Write("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine());
 
+if (num >= 10000 && num <= 99999)
+{
+    string Polindrom(int i)
+{
+    if (i / 10000 == i % 10 & (i / 1000) % 10 == (i % 100) / 10)
+    {
+        return "является полиндроном";
+
+    }
+    return " не является полидроном";
+}
+    {
+        Console.WriteLine("Число " + Polindrom(num));
+    }
+
+}
+else
+{
+    Console.WriteLine(" Error ");
+}
